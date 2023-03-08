37b49f9daece27c32104dbb277f92b1bc7d35cb0
Xrefullx <pyotr_gorbunov_01@mail.ru>
2023-03-08 22:52:49 +0700

    add memstorage

---
+++ internal/api/container/cont.go
@@ -0,0 +1,53 @@
+package container
+
+import (
+	"github.com/Xrefullx/YandexDiplom3/internal/models"
+	"github.com/Xrefullx/YandexDiplom3/internal/storage"
+	"github.com/Xrefullx/YandexDiplom3/internal/storage/memstorage"
+	"github.com/Xrefullx/YandexDiplom3/internal/storage/pg"
+
+	_ "github.com/lib/pq"
+	"github.com/sarulabs/di"
+	"go.uber.org/zap"
+)
+
+var DiContainer di.Container
+
+func ContainerBuild(cfg models.Config, logger *zap.Logger) error {
+	builder, err := di.NewBuilder()
+	if err != nil {
+		return err
+	}
+	var LoyalityStorage storage.LoyalityStorage
+	if cfg.DataBaseURI != "" {
+		LoyalityStorage, err = pg.New(cfg.DataBaseURI)
+		if err != nil {
+			return err
+		}
+	} else {
+		LoyalityStorage, err = memstorage.New()
+		if err != nil {
+			return err
+		}
+	}
+	if err = LoyalityStorage.Ping(); err != nil {
+		return err
+	}
+	if err = builder.Add(di.Def{
+		Name:  "server-config",
+		Build: func(ctn di.Container) (interface{}, error) { return cfg, nil }}); err != nil {
+		return err
+	}
+	if err = builder.Add(di.Def{
+		Name:  "zap-logger",
+		Build: func(ctn di.Container) (interface{}, error) { return logger, nil }}); err != nil {
+		return err
+	}
+	if err = builder.Add(di.Def{
+		Name:  "storage",
+		Build: func(ctn di.Container) (interface{}, error) { return LoyalityStorage, nil }}); err != nil {
+		return err
+	}
+	DiContainer = builder.Build()
+	return nil
+}
